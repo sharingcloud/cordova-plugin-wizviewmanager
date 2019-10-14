e598fedad140fb2e6e45e4383e46fcf7094baee4
Denis Bourge <denis.bourge@sharingcloud.com>
2019-10-14 18:48:40 +0200

    fix: oops

--- src/android/WizWebView.java
+++ src/android/WizWebView.java
@@ -43,8 +43,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 @SuppressLint("SetJavaScriptEnabled")
